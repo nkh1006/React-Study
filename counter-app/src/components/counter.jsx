52c31aac880fb7676a44c03b5a4d904e0204339e
NANGGI <mqsora@naver.com>
2021-05-14 17:37:50 +0900

    Add files via upload

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,28 +5,33 @@ class Counter extends Component {
         count: 0
     };
 
-    handleIncrement= () => {
+    handleIncrement = product => {
         this.setState({ count: this.state.count + 1 });
-    }
-
-    render() { 
-        return (
-        <div>
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
-        </div>
-        );
-    }
-
-    getBadgeClasses() {
-        let classes = "badge m-2 ";
-        classes += (this.state.count === 0) ? "badge-warning" : "badge-primary";
-        return classes;
     }
 
     formatCount() {
         const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
+        return count === 0 ? "Zero" : this.state.count;
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count ===0) ? "warning" : "primary";
+        return classes;
+    }
+
+    render() {
+        return (
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button 
+                    onClick={() => this.handleIncrement(product)} 
+                    className="btn btn-secondary btn-sm"
+                >
+                    Increment
+                </button>
+            </div>
+        ); 
     }
 }
  
