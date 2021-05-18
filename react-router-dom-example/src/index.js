5346eee32a608ea6dbb150d6548cfba64578a626
NANGGI <mqsora@naver.com>
2021-05-18 18:20:03 +0900

    Add files via upload

---
+++ react-router-dom-example/src/index.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Route, Switch, Link, NavLink, HashRouter, useParams} from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  )
+}
+
+var contents = [
+  {id:1, title:'HTML', description:'HTML is ...'},
+  {id:2, title:'JS', description:'JS is ...'},
+  {id:3, title:'React', description:'React is ...'}
+]
+
+function Topic() {
+  var params = useParams();
+  var topic_id = params.topic_id;
+  var selected_topic = {
+    title: 'Sorry',
+    description: 'Not Found'
+  }
+
+  for(var i=0; i<contents.length; i++) {
+    if(contents[i].id === Number(topic_id)) {
+      selected_topic = contents[i];
+      break;
+    }
+  }
+
+  return (
+    <div>
+      <h3>{selected_topic.title}</h3>
+      {selected_topic.description}
+    </div>
+  )
+}
+
+function Topics() {
+  var lis = [];
+  for(var i=0; i<contents.length; i++) {
+    lis.push(<li key={contents[i].id}><NavLink to={'/topics/'+contents[i].id}>{contents[i].title}</NavLink></li>)
+  }
+
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        {lis}
+      </ul>
+      <Route path="/topics/:topic_id">
+        <Topic />
+      </Route>
+    </div>
+  )
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>React Router DOM Example</h1>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/topics">Topics</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+      </ul>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/topics">
+          <Topics />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </React.Fragment>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
