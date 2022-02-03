83a2d2550aa134a241015669918f558b3231fe5d
NANGGI <mqsora@naver.com>
2022-02-03 16:33:00 +0900

    Add files via upload

---
+++ node_react_app/frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Nav from './components/Nav';
+import Home from'./components/Home';
+import Tweet from'./components/Tweet';
+import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Nav />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/tweets" exact component={Tweet} />
+          </Switch>        
+        </header>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ node_react_app/frontend/src/components/Tweet.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react';
+
+const Tweet = () => {
+  useEffect( () => {
+    fetchItems();
+  }, []);
+
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const data = await fetch('/tweets');
+    const items = await data.json();
+    setItems(items);
+  };
+
+  return (
+    <section>
+      {
+        items.map(item => (
+          <div class="container-fluid p-3 w-50">
+            <div class="card-deck">
+              <div class="card">
+                <div class="card-body p-1">
+                  <h6 class="card-title">{item.name}</h6>
+                  <p class="card-text">{item.msg}</p>
+                  <p class="card-text"><i>by {item.username}</i></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))
+      }
+    </section>
+  );
+}
+
+export default Tweet;
