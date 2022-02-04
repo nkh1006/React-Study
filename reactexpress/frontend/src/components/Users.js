a10627349c5f7dab34a0bbbc969bd86c4d499f7e
NANGGI <mqsora@naver.com>
2022-02-04 16:00:28 +0900

    Add files via upload

---
+++ reactexpress/frontend/src/components/Users.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/users/").then(res => {
+      if(res.ok) {
+        return res.json()
+      }
+    }).then(jsonRes => setUsers(jsonRes.usersList))
+  }, []);
+
+  return (
+    <div>
+      {users.map(user => (
+        <li>{user}</li>
+      ))}
+    </div>
+  )
+};
+
+export default Users;
