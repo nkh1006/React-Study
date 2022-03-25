26cb29f0d45ad05bfbafb621fdb003317c4334d0
NANGGI <mqsora@naver.com>
2022-03-25 11:49:10 +0900

    Add files via upload

--- react-socket-tut/server/index.js
+++ react-socket-tut/server/index.js
@@ -7,13 +7,26 @@ const cors = require("cors");
 app.use(cors());
 
 const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
-server.listen(3000, () => {
-  console.log("Server is running");
-})
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
