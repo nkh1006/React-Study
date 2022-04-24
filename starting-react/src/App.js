5ce634710b391c93197ce9539c0a19d062abb608
NANGGI <mqsora@naver.com>
2022-04-25 00:48:26 +0900

    Add files via upload

---
+++ starting-react/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import pokemon from './pokemon.json';
+
+const PokemonRow = ({ pokemon }) => (
+  <tr
+    key={pokemon.id}
+  >
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(", ")}</td>
+  </tr>
+);
+
+function App() {
+  return (
+    <div
+      style={{
+        margin: 'auto',
+        width: 800,
+        paddingTop: '1rem'
+      }}
+    >
+      <h1 className='title'>Pokemon Search</h1>
+      <table width='100%'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemon.slice(0, 20).map((pokemon) => (
+            <PokemonRow pokemon={pokemon} key={pokemon.id} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
